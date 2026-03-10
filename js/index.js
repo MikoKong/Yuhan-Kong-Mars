827daffaac50a157f05fc9abb9ec299cbf6712f7
mikokyh <mikokyh@gmail.com>
2026-03-10 16:44:39 -0700

    Final updates

--- js/index.js
+++ js/index.js
@@ -2,8 +2,8 @@ const body = document.querySelector("body");
 const footer = document.createElement("footer");
 body.appendChild(footer);
 
-const tody = new Date();
-const thisYear = tody.getFullYear();
+const today = new Date();
+const thisYear = today.getFullYear();
 
 const copyright = document.createElement("p");
 copyright.innerHTML = `© Yuhan Kong ${thisYear}`
@@ -14,7 +14,7 @@ const skills = [
     "HTML",
     "CSS",
     "Python"];
-const skillsSection = document.querySelector("#skills");
+const skillsSection = document.querySelector("#Skills");
 const skillsList = skillsSection.querySelector("ul");
 for (let i = 0; i < skills.length; i++) {
     const skill = document.createElement("li");
@@ -41,14 +41,32 @@ messageForm.addEventListener('submit', (event) => {
         <span>${message}</span>`;
     
     const removeButton = document.createElement("button");
-    removeButton.innerHTML = "remove";
+    removeButton.innerHTML = "Remove";
     removeButton.type = "button";
     removeButton.addEventListener("click", (event) => {
         const entry = event.target.parentNode;
         entry.remove();
     });
 
+    const editButton = document.createElement("button");
+    editButton.innerHTML = "Edit";
+    editButton.type = "button";
+    editButton.addEventListener("click", (event) => {
+        const entry = event.target.parentNode;
+        const messageSpan = entry.querySelector("span");
+
+        if(messageSpan.isContentEditable) {
+            messageSpan.contentEditable = "false";
+            event.target.textContent = "Edit";
+        } else {
+            messageSpan.contentEditable = "true";
+            messageSpan.focus();
+            event.target.textContent = "Save";
+        }
+    });
+
     newMessage.appendChild(removeButton);
+    newMessage.appendChild(editButton);
     messageList.appendChild(newMessage);
     event.target.reset();
 });
@@ -68,7 +86,13 @@ fetch("https://api.github.com/users/MikoKong/repos")
 
         for(let i = 0; i < repositories.length; i++) {
             const project = document.createElement("li");
-            project.innerText = repositories[i].name;
+            const link = document.createElement("a");
+
+            link.href = repositories[i].html_url;
+            link.target = "_blank";
+            link.textContent = repositories[i].name;
+
+            project.appendChild(link);
             projectList.appendChild(project);
         }
 
